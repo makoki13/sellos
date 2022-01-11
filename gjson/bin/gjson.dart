71013a650ab7a8735a40c76e16f76bd546885f78
makoki13 <pablo.makoki@gmail.com>
2022-01-11 17:36:45 +0100

    creacion de build

--- gjson/bin/gjson.dart
+++ gjson/bin/gjson.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 import 'dart:convert' show utf8, LineSplitter;
+
 Future<void> main(List<String> arguments) async {
-  var ficheroOrigen = '../sellos_plantilla.json';  
+  var ficheroOrigen = '../sellos_plantilla_desarrollo.json';
   var ficheroDestino = 'sellos_test.json';
 
   final destino = File(ficheroDestino).openWrite(mode: FileMode.write);
@@ -9,23 +10,21 @@ Future<void> main(List<String> arguments) async {
       .bind(File(ficheroOrigen).openRead())
       .transform(const LineSplitter());
 
-  
-  try {    
+  try {
     bool lineaAnteriorEsJson = false;
-    await for (final linea in origen) {      
+    await for (final linea in origen) {
       if (linea.contains('.json') == true) {
         if (lineaAnteriorEsJson == true) {
-          guardaLineaSinCodificar(destino,",");
+          guardaLineaSinCodificar(destino, ",");
         }
-        await inoculaFicheroAnyos(destino,linea);
+        await inoculaFicheroSeries(destino, linea);
         lineaAnteriorEsJson = true;
-      }
-      else {      
-        guardaLineaSinCodificar(destino,linea);
+      } else {
+        guardaLineaSinCodificar(destino, linea);
         lineaAnteriorEsJson = false;
       }
     }
-  } catch (_) {    
+  } catch (_) {
     await _handleError(ficheroOrigen);
   }
 
@@ -34,45 +33,100 @@ Future<void> main(List<String> arguments) async {
 
 Future<void> _handleError(String path) async {
   stderr.writeln('error: $path');
-  
 }
 
-void guardaLineaSinCodificar(destino,linea) {
+void guardaLineaSinCodificar(destino, linea) {
   stdout.writeln(linea);
   destino.writeln(linea);
 }
 
-Future<void> inoculaFicheroAnyos(destino,linea) async {
+bool esEsquema(linea) {
+  return linea.contains('schema');
+}
+
+Future<void> inoculaFicheroSeries(destino, linea) async {
   String ficheroTrimmed = linea.toString().trim();
-  if (ficheroTrimmed.substring(1,7) != "schema") {
+  if (esEsquema(ficheroTrimmed.substring(1, 7)) == false) {
     String fichero = '';
-    if (ficheroTrimmed.substring(ficheroTrimmed.length - 1,ficheroTrimmed.length) == ',' ) {    
-      fichero = ficheroTrimmed.substring(1,ficheroTrimmed.length-2);
+    if (ficheroTrimmed.substring(
+            ficheroTrimmed.length - 1, ficheroTrimmed.length) ==
+        ',') {
+      fichero = ficheroTrimmed.substring(1, ficheroTrimmed.length - 2);
+    } else {
+      fichero = ficheroTrimmed.substring(1, ficheroTrimmed.length - 1);
     }
-    else {
-      fichero = ficheroTrimmed.substring(1,ficheroTrimmed.length-1);
-    }  
-    String anyo = fichero.substring(0,4);
+    String anyo = fichero.substring(0, 4);
     String ficheroOrigen = "../series/" + anyo + "/" + fichero;
+    String directorio = fichero.substring(0, fichero.length - 5);
+
+    //print(ficheroOrigen);
+
+    final origen = utf8.decoder
+        .bind(File(ficheroOrigen).openRead())
+        .transform(const LineSplitter());
+
+    try {
+      bool lineaAnteriorEsJson = false;
+      await for (final linea in origen) {
+        //print(lineaAnteriorEsJson.toString());
+        if (linea.contains('.json') == true) {
+          if (lineaAnteriorEsJson == true) {
+            guardaLineaSinCodificar(destino, ",");
+          }
+          await inoculaFicheroSellos(destino, anyo, directorio, linea);
+          if (esEsquema(linea) == false) {
+            lineaAnteriorEsJson = true;
+          }
+        } else {
+          //print(linea);
+          guardaLineaSinCodificar(destino, linea);
+        }
+      }
+    } catch (_) {
+      await _handleError(ficheroOrigen);
+    }
+  }
+}
+
+Future<void> inoculaFicheroSellos(destino, anyo, directorio, linea) async {
+  String ficheroTrimmed = linea.toString().trim();
+  if (ficheroTrimmed.substring(1, 7) != "schema") {
+    String fichero = '';
+    if (ficheroTrimmed.substring(
+            ficheroTrimmed.length - 1, ficheroTrimmed.length) ==
+        ',') {
+      fichero = ficheroTrimmed.substring(1, ficheroTrimmed.length - 2);
+    } else {
+      fichero = ficheroTrimmed.substring(1, ficheroTrimmed.length - 1);
+    }
+    //String anyo = fichero.substring(0, 4);
+    String ficheroOrigen =
+        "../series/" + anyo + "/" + directorio + "/" + fichero;
     //print("@" + ficheroOrigen + "@");
 
     final origen = utf8.decoder
         .bind(File(ficheroOrigen).openRead())
         .transform(const LineSplitter());
 
-    try {          
-      await for (final linea in origen) {        
+    try {
+      //bool lineaAnteriorEsJson = false;
+      await for (final linea in origen) {
         if (linea.contains('.json') == true) {
-          //inoculaFicheroAnyos(destino,linea);
-          guardaLineaSinCodificar(destino,linea);
-        }
-        else {      
+          //if (lineaAnteriorEsJson == true) {
+          //  guardaLineaSinCodificar(destino, ",");
+          //}
+          //inoculaFicheroSellos(destino, linea);
+          guardaLineaSinCodificar(destino, linea);
+          //lineaAnteriorEsJson = true;
+        } else {
           //print(linea);
-          guardaLineaSinCodificar(destino,linea);
+          guardaLineaSinCodificar(destino, linea);
         }
       }
-    } catch (_) {      
+    } catch (_) {
       await _handleError(ficheroOrigen);
     }
+  } else {
+    guardaLineaSinCodificar(destino, linea);
   }
 }
---
+++ preview/web/main.dart
@@ -0,0 +1,5 @@
+import 'dart:html';
+
+void main() {
+  querySelector('#output').text = 'Tu Dart app is running.';
+}
